ee449eae9da860ee4876335912d88ed12f557dad
PortgasXDXMajd <PortgasDMajd3398@gmail.com>
2022-06-10 18:34:04 +0400

    init

---
+++ Worker.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class WorkerService : BackgroundService
+    {
+        private const int generalDelay = 1 * 1000; // 10 seconds
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(generalDelay, stoppingToken);
+                await DoBackupAsync();
+            }
+        }
+
+        private static Task DoBackupAsync()
+        {
+            // Generate the B2B File
+
+            if (true)
+            {
+                Console.WriteLine("Executing background task");
+            }
+
+            return Task.FromResult("Done");
+        }
+    }
+}
